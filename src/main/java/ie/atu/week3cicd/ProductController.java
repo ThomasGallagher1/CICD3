ef8828e7ad53ab28b84f2b38f355917b57ea30eb
G00415457@atu.ie <G00415457@atu.ie>
2024-10-02 15:52:37 +0100

    added GET POST PUT DELETE

---
+++ src/main/java/ie/atu/week3cicd/ProductController.java
@@ -0,0 +1,49 @@
+package ie.atu.week3cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.ArrayList;
+@RestController
+public class ProductController {
+    private List<Product> products = new ArrayList<>();
+    public ProductController() {
+        products.add(new Product("Tv", "Made by Sony", 899, 100));
+        products.add(new Product("Radio", "Made by Samsung", 99, 101));
+    }
+
+    @GetMapping("/getProducts")
+    public List<Product> getProducts()
+    {
+        return products;
+    }
+    @PostMapping("/addProduct")
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
+        products.add(product);
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("?{id}")
+    public ResponseEntity<List> updateProduct(@PathVariable int id, @RequestBody Product product)
+    {
+        for(int i = 0; i < products.size(); i++){
+            if(products.get(i).getId() == id){
+                products.set(i, product);
+            }
+        }
+        return ResponseEntity.ok(products);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List> removeProduct(@PathVariable int id)
+    {
+        for(int i = 0; i < products.size(); i++){
+            if(products.get(i).getId() == id){
+                products.remove(i);
+            }
+        }
+        return ResponseEntity.ok(products);
+    }
+}
